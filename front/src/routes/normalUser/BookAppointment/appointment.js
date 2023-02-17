fe9d46b4e95b67099a46324fab1156dae860ba85
Hoang Bui <hbui6373@uni.sydney.edu.au>
2023-02-17 19:36:18 +1100

    Book Appointment Dialog and Date Time Converter

--- front/src/routes/normalUser/BookAppointment/appointment.js
+++ front/src/routes/normalUser/BookAppointment/appointment.js
@@ -19,18 +19,59 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../../../components/navbar/NavBar"
-
+import moment from 'moment';
 import { render } from "@testing-library/react";
 import { useState } from "react";
-import DatePicker from "react-datepicker";
+import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
 import "react-datepicker/dist/react-datepicker.css";
 function  Appointment() 
 {
     const [appointmentDescription, setAppointmentDescription] = useState("");
+    const [appointmentDateTimeValue, setAppointmentDateTimeValue] = useState("");
+    const [isPastDateDialogOpen, setIsPastDateDialogOpen] = useState(false);
+    const [dialogTitle, setDialogTitle] = useState("");
+    const [dialogContent, setDialogContent] = useState("");
+
     const [startDate, setStartDate] = useState(new Date());
+    const handlePastDate = () => {
+        setIsPastDateDialogOpen(true);
+    };
     const handleSubmit = () =>
     {
+         //Todo: Validate Current Date time picker value
+        const dateTimeObj = moment(appointmentDateTimeValue.appointmentDateTimeValue);
+        const date = dateTimeObj.format('MMMM Do YYYY');
+        const time = dateTimeObj.format('HH:mm');
+        if (dateTimeObj.isBefore())
+        {
+            //Todo: Open Dialog For Invalid Date Time
+            setDialogTitle("Invalid Date Time");
+            setDialogContent(
+                <Typography>
+                <strong>{date}</strong> at <strong>{time}</strong> is already in the past. Please select a future date and time.
+                </Typography>
+            );
+            handlePastDate();
+        }
+        else if (dateTimeObj.isAfter())
+        {
+            //Todo: Process Request
+            
+            //Todo: Send Backend Request To Make Appointment 
+        }
+        else 
+        {
+            //Todo: Open Dialog For Urgent Date Time
+            setDialogTitle("Urgent Appointment");
+            setDialogContent(
+                <Typography>
+                You have selected an appointment for <strong>{date}</strong> at <strong>{time}</strong>. Are you sure you want to book this appointment?
+                </Typography>
+            );
+            handlePastDate();
+
+        }
 
     }
     return ( 
@@ -78,6 +119,10 @@ function  Appointment()
                         label="Choose Your Appointment"
                         type="datetime-local"
                         // defaultValue="2017-05-24T10:30"
+                        onChange={(e) => {
+                            setAppointmentDateTimeValue(prevState => ({...prevState, appointmentDateTimeValue: e.target.value}));
+                            console.log(appointmentDateTimeValue);
+                            }}
                         sx={{ width: 500 }}
                         InputLabelProps={{
                             shrink: true,
@@ -109,8 +154,7 @@ function  Appointment()
                     {/**NOTE - Button Confirmation */}
                     <div>
                         <Button
-                            type="submit"
-                            
+                            onClick={handleSubmit}
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
                         >
@@ -120,6 +164,15 @@ function  Appointment()
                 </Box>
             </div>
         </Stack>
+        <Dialog open={isPastDateDialogOpen} onClose={() => setIsPastDateDialogOpen(false)}>
+            <DialogTitle>{dialogTitle}</DialogTitle>
+            <DialogContent>{dialogContent}</DialogContent>
+            <DialogActions>
+                <Button onClick={() => setIsPastDateDialogOpen(false)}>OK</Button>
+            </DialogActions>
+        </Dialog>
+        
+
         </>
     )
 }
